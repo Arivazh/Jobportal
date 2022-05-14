8edd78de9b02fe72239d262ea28f5d96383cac76
keerthikaRamu <baytone2021@gmail.com>
2022-05-14 18:18:20 +0530

    created Login and Register Page button as dynamically working

--- User/UserMaster.Master.cs
+++ User/UserMaster.Master.cs
@@ -9,9 +9,47 @@ namespace OnlineJobPortal.User
 {
     public partial class UserMaster : System.Web.UI.MasterPage
     {
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["user"] != null)
+            {
+                lbRegisterOrProfile.Text = "Profile";
+                lbLoginOrLogout.Text = "Logout";
 
+            }
+            else
+            {
+                lbRegisterOrProfile.Text = "Register";
+                lbLoginOrLogout.Text = "Login";
+            }
         }
+
+
+        protected void lbRegisterOrProfile_Click(object sender, EventArgs e)
+        {
+            if (lbRegisterOrProfile.Text == "Profile")
+            {
+                Response.Redirect("Profile.aspx");
+            }
+            else
+            {
+                Response.Redirect("Register.aspx");
+            }
+        }
+        protected void lbLoginOrLogout_Click(object sender, EventArgs e)
+        {
+            if (lbLoginOrLogout.Text == "Profile")
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                Session.Abandon();
+                Response.Redirect("Login.aspx");
+            }
+        }
+
     }
 }
